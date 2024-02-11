481f1c03ffaf528de74f48eb6dcedfd660f1b986
Charles Bon Chua <chuacharlesbon@gmail.com>
2024-02-11 16:46:42 +0800

    Added 404 Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,5 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 export const routes: Routes = [
     { path: '', component: HomeComponent, data: { title: 'Home' } },
     { path: 'about', component: AboutComponent, data: { title: 'About Us' } },
-    { path: '*', component: PageNotFoundComponent, data: { title: '404' } },
+    { path: '**', component: PageNotFoundComponent, data: { title: '404' } },
 ];
